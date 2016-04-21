02fa72208991319bed0a599092b6e18326cb4bb9
David Nolan <naviddolan@yahoo.com>
2016-04-21 16:26:23 +0100

    Removed unnecessary import

--- src/main/java/com/example/webservice/controller/GreetingController.java
+++ src/main/java/com/example/webservice/controller/GreetingController.java
@@ -6,7 +6,6 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
