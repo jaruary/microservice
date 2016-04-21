750cb7acee9b408749065458e1f9f22c6e2ae567
David Nolan <naviddolan@yahoo.com>
2016-04-21 16:25:17 +0100

    Changed annotation to be @RestController

--- src/main/java/com/example/webservice/controller/GreetingController.java
+++ src/main/java/com/example/webservice/controller/GreetingController.java
@@ -4,12 +4,12 @@ import com.example.data.entity.Greeting;
 import java.util.concurrent.atomic.AtomicLong;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class GreetingController {
   
   private static final Log LOG = LogFactory.getLog(GreetingController.class);
@@ -18,7 +18,6 @@ public class GreetingController {
   private final AtomicLong counter = new AtomicLong();
 
   @RequestMapping("/greeting")
-  @ResponseBody
   public Greeting greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
     LOG.info( String.join(" ", "Greeting Called with the name", name) );
     return new Greeting(counter.incrementAndGet(), String.format(template, name));
