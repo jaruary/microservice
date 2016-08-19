2a4dc3ded1545401c4640baa07141eed59042ab5
David Nolan <naviddolan@yahoo.com>
2016-08-19 17:31:39 +0100

    - Adding Flywaydb to the project. Run 'gradle flywayMigrate -i'.

---
+++ src/main/resources/db/migration/V1__Create_customer_table.sql
@@ -0,0 +1,10 @@
+CREATE SCHEMA IF NOT EXISTS `microservice`;
+SET SCHEMA `microservice`;
+
+DROP TABLE IF EXISTS `customer`;
+
+CREATE TABLE IF NOT EXISTS `customer` (
+    `id` BIGINT AUTO_INCREMENT NOT NULL,
+    `firstname` VARCHAR(255) NOT NULL,
+    `lastname` VARCHAR(255) NOT NULL
+);
