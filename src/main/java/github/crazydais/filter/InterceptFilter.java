d8bc28896c409c891589fc51dca5fb85f449bd13
David Nolan <naviddolan@yahoo.com>
2016-10-18 08:54:46 +0100

    Adding JWT to the project.

--- src/main/java/github/crazydais/filter/InterceptFilter.java
+++ src/main/java/github/crazydais/filter/InterceptFilter.java
@@ -1,54 +1,42 @@
 package github.crazydais.filter;
 
-import java.io.IOException;
-import java.util.Date;
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureException;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.web.filter.GenericFilterBean;
 
-public class InterceptFilter implements Filter {
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+public class InterceptFilter extends GenericFilterBean {
 
   private final Log log = LogFactory.getLog(InterceptFilter.class);
   private FilterConfig config = null;
 
-  public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws ServletException, IOException {
+    @Override
+    public void doFilter(final ServletRequest req, final ServletResponse res,  final FilterChain chain) throws IOException, ServletException {
+      final HttpServletRequest request = (HttpServletRequest) req;
 
-    HttpServletRequest request = (HttpServletRequest) req;
-    HttpServletResponse response = (HttpServletResponse) res;
+      final String authHeader = request.getHeader("Authorization");
+      if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+        throw new ServletException("Missing or invalid Authorization header.");
+      }
 
-    // Get the IP address of client machine.   
-    String ipAddress = request.getRemoteAddr();
+      final String token = authHeader.substring(7);
 
-    Cookie reqCookies[] = request.getCookies();
+      try {
+        final Claims claims = Jwts.parser().setSigningKey("secretkey")
+                .parseClaimsJws(token).getBody();
+        request.setAttribute("claims", claims);
+      }
+      catch (final SignatureException e) {
+        throw new ServletException("Invalid token.");
+      }
 
-    Cookie customCookie = new Cookie("custom-cookie-key", "custom-cookie-value");
-    customCookie.setDomain(req.getServerName());
-    customCookie.setPath(request.getContextPath());
-    customCookie.setSecure(request.isSecure());
-    customCookie.setHttpOnly(false);
-    response.addCookie(customCookie);
-
-    // Log the IP address and current timestamp.
-    log.info("IP " + ipAddress + ", Time " + new Date().toString());
-
-    chain.doFilter(req, res);
-  }
-
-  public void init(FilterConfig config) throws ServletException {
-    this.config = config;
-    config.getServletContext().log("Initializing InterceptFilter");
-  }
-
-  public void destroy() {
-    config.getServletContext().log("Destroying InterceptFilter");
-  }
+      chain.doFilter(req, res);
+    }
 
 }
