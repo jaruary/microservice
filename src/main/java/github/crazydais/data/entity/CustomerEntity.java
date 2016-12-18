2b47934f2b83bf77070bdca0105194a428d4fa27
David Nolan <naviddolan@yahoo.com>
2016-12-18 18:15:20 +0000

    Removed file controller as I want to keep this project clean from unnecessary code.  The customer controller is good on it's own as an example.

--- src/main/java/github/crazydais/data/entity/CustomerEntity.java
+++ src/main/java/github/crazydais/data/entity/CustomerEntity.java
@@ -1,21 +1,12 @@
 package github.crazydais.data.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import java.util.List;
 
 @Entity
 @Table(name = "customer", catalog = "microservice")
 public class CustomerEntity extends BaseEntity {
-
-    @OneToMany(fetch = FetchType.LAZY, mappedBy = "customer")
-    @JsonIgnore
-    private List<FileEntity> files;
 
     @Column(name = "firstname", nullable = false)
     private String firstName;
@@ -41,16 +32,6 @@ public class CustomerEntity extends BaseEntity {
     public void setLastName(String lastName) {
 
         this.lastName = lastName;
-    }
-
-    public List<FileEntity> getFiles() {
-
-        return this.files;
-    }
-
-    public void setFiles(List<FileEntity> files) {
-
-        this.files = files;
     }
 
 
--- src/main/resources/db/migration/V4__Add_files.sql
+++
@@ -1,6 +0,0 @@
-SET SCHEMA `microservice`;
-
-INSERT INTO `file` (`created`, `extension`, `filedata`, `name`, `updated`, `customerid`)
-VALUES
-  (CURRENT_TIMESTAMP(), 'txt', '54686973206973206120746578742066696c652e2e2e',
-   'myfile', CURRENT_TIMESTAMP(), 1);
